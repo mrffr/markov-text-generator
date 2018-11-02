00966fb5c0817e1ca4706922de815e2506180c11
F.Fitzgerald <mrffitzgerald@runbox.com>
2018-11-02 22:43:00 +0000

    setting up files

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+import markov_text_gen
+
+class TestMarkovText(unittest.TestCase):
+    def test_input(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
